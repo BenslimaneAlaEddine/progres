99f92c8db9092f9d446864a06c624a9e73f773cf
AlaEddine <alilotoula610@gmail.com>
2025-05-29 22:35:41 +0100

    initial commit

---
+++ lib/login.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginL extends StatelessWidget {
+  const LoginL({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return LoginF(
+      size: size,
+    );
+  }
+}
+
+class LoginF extends StatefulWidget {
+  const LoginF(
+      {super.key,
+      required this.size}); // lokan hna madernach required talaelina err
+  // pcq size lazm tkon eandha valeur pcq fl dec rana dayrin "Size size;" machi "Size? size"
+  final Size size;
+
+  @override
+  LoginS createState() => LoginS();
+}
+
+class LoginS extends State<LoginF> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFD3D3D3),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: widget.size.width * 0.4,
+            left: widget.size.width / 2 - (widget.size.width * 0.7 / 2),
+            right: widget.size.width / 2 - (widget.size.width * 0.7 / 2),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                  height: widget.size.width * 0.3,
+                  width: widget.size.width * 0.7,
+                  child: Image.asset(
+                    "images/progres.png",
+                    fit: BoxFit.contain,
+                  )),
+              SizedBox(
+                height: widget.size.height * 0.1,
+              ),
+              const Text("Progiciel de Gestion Intégré"),
+              const Text("بوابة الطالب"),
+              SizedBox(
+                height: 20,
+              ),
+              Form(
+                child: Column(
+                  children: [
+                    TextField(
+                      style: TextStyle(height: 1),
+                      decoration: InputDecoration(
+                          label: Row(
+                            children: [
+                              Icon(Icons.account_circle),
+                              Text("ادخل رقم التسجيل")
+                            ],
+                          ),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 1),
+                              borderRadius: BorderRadius.circular(30))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      style: TextStyle(height: 1),
+                      decoration: InputDecoration(
+                          label: Row(
+                            children: [Icon(Icons.key), Text("ادخل كلمة المرور")],
+                          ),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 1),
+                              borderRadius: BorderRadius.circular(30))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "تسجيل الدخول",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              const WidgetStatePropertyAll(Colors.green),
+                          padding: WidgetStatePropertyAll(EdgeInsets.only(
+                            top: 10,
+                            bottom: 10,
+                            left: widget.size.width / 2 -
+                                (widget.size.width * 0.7 / 2),
+                            right: widget.size.width / 2 -
+                                (widget.size.width * 0.7 / 2),
+                          ))),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text("وزارة التعليم العالي والبحث العلمي"),
+              SizedBox(
+                height: 10,
+              ),
+              Text("جميع الحقوق محفوظة © 2024"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:progres/login.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginL(),
+    );
+  }
+}
