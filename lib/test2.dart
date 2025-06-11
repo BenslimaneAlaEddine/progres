cf1d4c1912edc6f2ff0313d7806ba24962cde3c1
AlaEddine <alaeddineben181@gmail.com>
2025-06-11 20:23:13 +0100

    Just try out ideas, turn the card over in the 'test 2' file.

--- lib/test2.dart
+++ lib/test2.dart
@@ -4,22 +4,46 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Test2 extends StatelessWidget {
-   ValueChanged<DragUpdateDetails >? c;
-   // double? a;
-   double x;
-   double y;
-  Test2({super.key,required this.c,required this.x,required this.y});
+  ValueChanged<DragUpdateDetails>? c;
+  GestureDragEndCallback? g;
+  double angle;
+bool isFont;
+  // double x;
+  // double y;
+  Test2({
+    super.key,
+    required this.c,
+    required this.g,
+    required this.angle,
+    required this.isFont
+  });
+
   @override
   Widget build(BuildContext context) {
+    // angle=0;
     return Scaffold(
       body: Center(
-        child: Transform(alignment: Alignment.center,
-          transform: Matrix4.identity()..translate(x,y),
-          child: GestureDetector(onHorizontalDragUpdate: c,
-            child: Container(
+        child: Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.identity()
+            ..setEntry(3, 2, 0.001)
+            ..rotateY(angle),
+          child: GestureDetector(
+            onHorizontalDragEnd: g,
+            onHorizontalDragUpdate: c,
+            child: isFont ? Container(
               height: 100,
               width: 100,
-              color: Colors.red,child: Text("data"),
+              color: Colors.red,
+              child: const Text("Font"),
+            ) : Transform(alignment: Alignment.center,
+              transform: Matrix4.rotationY(pi),
+              child: Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+                child: const Text("Back."),
+              ),
             ),
           ),
         ),
